6aab0a235cf12f7f36f9a4aa1ba3b36e8ca0220a
wajeht <58354193+wajeht@users.noreply.github.com>
2024-02-25 22:26:29 -0600

    test: update test cases for JSON response and 404 message

--- src/index.test.js
+++ src/index.test.js
@@ -4,8 +4,21 @@ import { app as server } from './index.js';
 
 const app = request(server);
 
-it('should be able to ping healthz end pint', async () => {
+it('should send a JSON response', async () => {
+	const response = await app.get('/').set('Content-Type', 'application/json');
+	expect(response.status).toBe(200);
+	expect(response.headers['content-type']).toBe('application/json; charset=utf-8');
+	expect(response.body).toHaveProperty('ip');
+});
+
+it('should be able to ping healthz end point', async () => {
 	const response = await app.get('/healthz');
 	expect(response.status).toBe(200);
 	expect(response.body).toStrictEqual({ message: 'ok' });
+});
+
+it('should be able to hit not found', async () => {
+	const response = await app.get('/not-found');
+	expect(response.status).toBe(404);
+	expect(response.body).toStrictEqual({ message: 'not found' });
 });
