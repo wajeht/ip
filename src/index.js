657cff497c15a1253de4b5cc942bae5c315c0909
wajeht <58354193+wajeht@users.noreply.github.com>
2024-02-25 18:17:48 -0600

    feat: Update route handlers and error middleware

--- src/index.js
+++ src/index.js
@@ -12,12 +12,14 @@ app.use(cors());
 app.use(helmet());
 app.use(compression());
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
   console.log(ip);
   return res.send(ip + '\n');
 });
 
-app.get('/healthz', (req, res) => res.json({ message: "ok"}))
+app.get('/healthz', (req, res) => res.json({ message: "ok"}));
+app.use((req, res, next) => res.status(404).json({ message: 'not found'}));
+app.use((err, req, res, next) => res.status(500).json({ message: 'error'}));
 
-app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server was started on port http://localhost:${PORT}`));
