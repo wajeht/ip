4f7793b4a95368ed6cabf060832c4012c90010da
wajeht <58354193+wajeht@users.noreply.github.com>
2024-05-11 13:04:09 -0500

    test: Add @types/supertest and mock geoip-lite in tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,63 @@
 import request from 'supertest';
-import { it, expect } from 'vitest';
+import { it, expect, vi } from 'vitest';
 import { app as server } from './index.js';
+import geoIpLite from 'geoip-lite';
+
+vi.mock('geoip-lite');
 
 const app = request(server);
+
+it('should return with verbose and html', async () => {
+	const ip = '127.0.0.1';
+	const mockGeoData = {
+		range: [123456789, 123456789],
+		country: 'US',
+		region: 'CO',
+		eu: '1',
+		timezone: 'America/Chicago',
+		city: 'Main',
+		ll: [69.2023, -420.846],
+		metro: 1,
+		area: 0,
+	};
+
+	geoIpLite.lookup.mockReturnValueOnce(mockGeoData);
+
+	const response = await app.get(`/?verbose=true`).set('x-forwarded-for', ip);
+
+	expect(response.headers['content-type']).toBe('text/html; charset=utf-8');
+	expect(response.text).includes(ip);
+	expect(response.text).includes(`<strong>`);
+	expect(response.text).includes(mockGeoData.area);
+	expect(response.text).includes(mockGeoData.ll);
+	expect(geoIpLite.lookup).toHaveBeenCalledTimes(1);
+});
+
+it('should return with verbose and json', async () => {
+	const ip = '127.0.0.1';
+
+	const mockGeoData = {
+		range: [123456789, 123456789],
+		country: 'US',
+		region: 'CO',
+		eu: '1',
+		timezone: 'America/Chicago',
+		city: 'Main',
+		ll: [69.2023, -420.846],
+		metro: 1,
+		area: 0,
+	};
+
+	geoIpLite.lookup.mockReturnValueOnce(mockGeoData);
+
+	const response = await app.get(`/?verbose=true&json=true`).set('x-forwarded-for', ip);
+
+	expect(response.headers['content-type']).toBe('application/json; charset=utf-8');
+	expect(response.text).includes(ip);
+	expect(response.text).includes(mockGeoData.area);
+	expect(response.text).includes(mockGeoData.ll);
+	expect(geoIpLite.lookup).toHaveBeenCalledTimes(1);
+});
 
 it('should be able to get an ip address', async () => {
 	const response = await app.get('/').set('Content-Type', 'application/json');
