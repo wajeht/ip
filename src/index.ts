c834244a754d3f70955f90c824d5fd2d1d3e6111
wajeht <58354193+wajeht@users.noreply.github.com>
2024-05-11 13:21:46 -0500

    fix: Update express middleware options

--- src/index.ts
+++ src/index.ts
@@ -66,9 +66,9 @@ app.use(
 
 app.use(compression());
 
-app.use(express.json());
+app.use(express.json({ limit: '1mb' }));
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ limit: '1mb', extended: true }));
 
 app.use(express.static(path.resolve(path.join(process.cwd(), 'public')), { maxAge: '24h' }));
 
