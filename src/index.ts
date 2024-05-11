982c2bb8b025640c13373a23890af862f0c99429
wajeht <58354193+wajeht@users.noreply.github.com>
2024-05-11 12:25:20 -0500

    feat: Add helmet CSP directives and express middleware

--- src/index.ts
+++ src/index.ts
@@ -46,9 +46,29 @@ app.use(
 
 app.use(cors());
 
-app.use(helmet());
+app.use(
+	helmet({
+		contentSecurityPolicy: {
+			directives: {
+				...helmet.contentSecurityPolicy.getDefaultDirectives(),
+				'default-src': ["'self'", 'plausible.jaw.dev '],
+				'script-src': [
+					"'self'",
+					"'unsafe-inline'",
+					'dogs.jaw.dev',
+					'localhost',
+					'plausible.jaw.dev',
+				],
+			},
+		},
+	}),
+);
 
 app.use(compression());
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.resolve(path.join(process.cwd(), 'public')), { maxAge: '24h' }));
 
