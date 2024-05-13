6659f1699f7790062d28e67d6efa9ba62f66f5ea
Jaw <58354193+wajeht@users.noreply.github.com>
2024-05-12 23:12:41 -0500

    Update main.go

--- main.go
+++ main.go
@@ -30,20 +30,24 @@ func healthzHandler(w http.ResponseWriter, r *http.Request) {
 
 func getIPAddress(r *http.Request) string {
 	ipAddress := r.Header.Get("x-forwarded-for")
+	
 	if ipAddress == "" {
 		ipAddress, _, _ = net.SplitHostPort(r.RemoteAddr)
 	} else {
 		ips := strings.Split(ipAddress, ", ")
 		ipAddress = ips[0]
 	}
+	
 	return ipAddress
 }
 
 func LookupLocation(ipStr string) *geoip2.City {
 	db, err := geoip2.Open("GeoLite2-City.mmdb")
+	
 	if err != nil {
 		log.Fatal(err)
 	}
+	
 	defer db.Close()
 
 	ip := net.ParseIP(ipStr)
