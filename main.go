cb553f7c5f66b0e7dbfce35cd303a7e4739174b1
wajeht <58354193+wajeht@users.noreply.github.com>
2024-05-12 20:15:56 -0500

    feat: Add healthz and ip handlers

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+const PORT = 80
+
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("<html><body><span>ok</span></body></html>"))
+}
+
+func ipHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"status":"ok"}`))
+}
 
 func main() {
-	fmt.Print("testing")
+	http.HandleFunc("/healthz", healthzHandler)
+	http.HandleFunc("/", ipHandler)
+	fmt.Println("Server was started on http://localhost:", PORT)
+	http.ListenAndServe(fmt.Sprintf(":%d", PORT), nil)
 }
